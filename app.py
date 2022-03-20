e8717c9b1bb58a15f3be97af01704ec4f8001d47
ottonovak <xnovaks1@stuba.sk>
2022-03-20 18:45:10 +0100

    v2/matches nedokoncene

--- app.py
+++ app.py
@@ -30,24 +30,24 @@ def lin_establish_connection():
        password=var['DBSPASS'])
     return conn
 
-
-@app.route('/v1/health', methods=['GET'])
-def do_stuff():
-    print('Request for index page received')
-
+def establish_connection():
     if os.name == "nt":     # vrati nt pre widnows a posix pre linux a mac
         conn = win_establish_connection()
     else:
         conn = lin_establish_connection()
+    return conn
 
-
+@app.route('/v1/health', methods=['GET'])
+def v1_health():
+    print('Request for index page received')
+    conn = establish_connection()
     pointer = conn.cursor()
+
     pointer.execute("SELECT VERSION()")
     version = pointer.fetchone()
 
     pointer.execute("SELECT pg_database_size('dota2')/1024/1024 as dota2_db_size")
     size = pointer.fetchone()
-
 
     response = {}
     response2 = {}
@@ -55,9 +55,100 @@ def do_stuff():
     response["dota2_db_size"] = size[0]
     response['version'] = version[0]
 
-
     final_response = json.dumps(response2)
     return final_response
+
+
+@app.route('/v2/patches/', methods=['GET'])
+def v2_patches():
+    conn = establish_connection()
+    pointer = conn.cursor()
+
+    pointer.execute("SELECT all_paches.patch_version, all_paches.patch_start_date, all_paches.patch_end_date, matches.id AS match_id, duration "
+                    "FROM matches "
+                    "LEFT JOIN( "
+                    "SELECT p1.name as patch_version, "
+                    "extract(EPOCH from p1.release_date) as patch_start_date, "
+                    "extract(EPOCH from p2.release_date) as patch_end_date "
+                    "FROM patches as p1 "
+                    "LEFT JOIN patches as p2 "
+                    "ON p1.id = p2.id - 1 "
+                    "ORDER BY p1.id "
+                    ") AS all_paches "
+                    "ON matches.start_time > all_paches.patch_start_date AND matches.start_time < COALESCE(all_paches.patch_end_date, 9999999999)")
+
+    response = {}
+    response['patches'] = []
+
+    for row in pointer:
+        current_patch = None
+        for patch in response['patches']:
+            if patch['patch_version'] == str(row[0]):
+                current_patch = patch
+                break
+
+        if current_patch is not None:
+            match = {}
+            match['match_id'] = row[3]
+            match['duration'] = row[4]
+            current_patch['matches'].append(match)
+
+        else:
+            current_patch = {}
+            current_patch['patch_version'] = row[0]
+            current_patch['patch_start_date'] = row[1]
+            current_patch['patch_end_date'] = row[2]
+            current_patch['matches'] = []
+
+            match = {}
+            match['match_id'] = row[3]
+            match['duration'] = row[4]
+            current_patch['matches'].append(match)
+            response['patches'].append(current_patch)
+
+    return json.dumps(response)
+
+
+@app.route('/v2/players/<string:id>/game_exp/', methods=['GET'])
+def v2_game_exp(id):
+    conn = establish_connection()
+    pointer = conn.cursor()
+
+    player_dic = {}
+    player_dic['id'] = int
+
+
+    pointer.execute("SELECT p.id, "
+                    "COALESCE(p.nick,'unknown') AS player_nick, "
+                    "match_id, "
+                    "localized_name AS hero_localized_name, "
+                    "ROUND(m.duration/60.0, 2) AS match_duration_minutes, "
+                    "COALESCE(xp_hero, 0) + COALESCE(xp_creep,0) + COALESCE(xp_other,0) + COALESCE(xp_roshan,0) as experiences_gained, "
+                    "greatest(level) as level_gained, "
+                    "CASE WHEN mpd.player_slot < 5 AND m.radiant_win = 'true' OR mpd.player_slot >127 AND m.radiant_win = 'false' "
+                    "THEN 'true' ELSE 'false' END AS winner "
+                    "FROM matches_players_details as mpd "
+                    "JOIN players as p "
+                    "on p.id = mpd.player_id "
+                    "JOIN heroes as h "
+                    "on mpd.hero_id = h.id "
+                    "JOIN matches as m "
+                    "on mpd.match_id = m.id "
+                    "WHERE p.id =" + id +
+                    " ORDER BY m.id")
+
+    final_response = pointer.fetchone()
+    return final_response
+
+"""
+
+ak SQL vrati tabulku, treba pouzit
+
+for row in pointer:
+    print("Riadok: " + row[0] + " | " + row[1]...)
+
+"""
+
 
 
 
